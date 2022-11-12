7a30cf4ba094217853b9a21f03357964d6cab8d5
Nic Waller <code@nicwaller.com>
2022-11-12 15:49:38 -0800

    day 11, part 2

--- day11/day11.go
+++ day11/day11.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"advent-of-code/lib/aoc"
+	"advent-of-code/lib/assert"
 	"advent-of-code/lib/grid"
 	"advent-of-code/lib/iter"
 	"fmt"
@@ -26,14 +27,17 @@ func part1(g grid.Grid[int]) int {
 	incr := func(v int) int { return v + 1 }
 
 	//For 100 steps
-	flashes := 0
-	for step := 1; step <= 100; step++ {
+	firstSynchrony := -1
+	flashesInFirst100Steps := 0
+Forever:
+	for step := 1; ; step++ {
 		// First, the energy level of each octopus increases by 1.
 		g.MapAll(incr)
 
-		// Then, any octopus with an energy level greater than 9 flashes.
+		// Then, any octopus with an energy level greater than 9 flashesInFirst100Steps.
 		// This increases the energy level of all adjacent octopuses by 1,
 		// including octopuses that are diagonally adjacent.
+		flashesInStep := 0
 		flashgrid.Fill(false)
 		var anyDidFlash bool
 		for {
@@ -46,8 +50,11 @@ func part1(g grid.Grid[int]) int {
 						// it already flashed.
 						continue
 					}
-					// the octopus flashes!!!
-					flashes++
+					// the octopus flashesInFirst100Steps!!!
+					if step <= 100 {
+						flashesInFirst100Steps++
+					}
+					flashesInStep++
 					flashgrid.Set(cell, true)
 					anyDidFlash = true
 					neighbours := g.NeighboursSurround(cell, true)
@@ -57,6 +64,11 @@ func part1(g grid.Grid[int]) int {
 			if anyDidFlash {
 				continue
 			} else {
+				if flashesInStep == g.RowCount()*g.ColumnCount() {
+					fmt.Println("synchrony acheived")
+					firstSynchrony = step
+					break Forever
+				}
 				break
 			}
 		}
@@ -65,15 +77,17 @@ func part1(g grid.Grid[int]) int {
 		for tiredOcto := flashgrid.Filter(func(didFlash bool) bool { return didFlash }); tiredOcto.Next(); {
 			g.Set(tiredOcto.Value(), 0)
 		}
-		fmt.Printf("After step %d:\n", step)
-		g.Print()
+		//fmt.Printf("After step %d:\n", step)
+		//g.Print()
 	}
 	//  - grid.items.filter(>9 and not flashed)
 	//  - break if no result
 	//  - flashNeighbours(diag=yes)
 	// - grid.items.filter(>9).map(zero)
-	//assert.EqualAny(riskSum, []int{15, 516}, "riskSum")
-	return flashes
+	assert.EqualAny(flashesInFirst100Steps, []int{1640}, "flashesInFirst100Steps")
+	assert.EqualAny(firstSynchrony, []int{312}, "firstSynchrony")
+	fmt.Printf("synchrony in step %d\n", firstSynchrony)
+	return flashesInFirst100Steps
 }
 
 func part2(g grid.Grid[int]) int {
