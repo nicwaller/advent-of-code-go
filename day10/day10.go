33cdc0e5babf10732dfc87eeb0c16c708ccf02e9
Nic Waller <code@nicwaller.com>
2022-11-13 08:25:13 -0800

    day 10 part 2

--- day10/day10.go
+++ day10/day10.go
@@ -5,11 +5,12 @@ import (
 	"advent-of-code/lib/assert"
 	"advent-of-code/lib/f8l"
 	"fmt"
+	"sort"
 	"strings"
 )
 
 func main() {
-	//aoc.UseSampleFile()
+	aoc.UseSampleFile()
 	fmt.Printf("Part 1: %d\n", part1(parseFile()))
 	fmt.Printf("Part 2: %d\n", part2(parseFile()))
 }
@@ -27,7 +28,7 @@ func firstSyntaxError(line string) rune {
 	}
 	deque := make([]rune, 100)
 	dPtr := -1
-	for i, c := range line {
+	for _, c := range line {
 		if strings.ContainsAny(string(c), "([{<") {
 			// opening match
 			dPtr++
@@ -39,12 +40,47 @@ func firstSyntaxError(line string) rune {
 			if actual == expected {
 				dPtr--
 			} else {
-				fmt.Printf("error in col %d (%v != %v)\n", i, string(actual), string(expected))
+				//fmt.Printf("error in col %d (%v != %v)\n", i, string(actual), string(expected))
 				return c
 			}
 		}
 	}
 	return ' '
+}
+
+func autocomplete(line string) []rune {
+	match := map[rune]rune{
+		'(': ')',
+		'[': ']',
+		'{': '}',
+		'<': '>',
+	}
+	deque := make([]rune, 100)
+	dPtr := -1
+	for _, c := range line {
+		if strings.ContainsAny(string(c), "([{<") {
+			// opening match
+			dPtr++
+			deque[dPtr] = match[c]
+		} else {
+			// should be closing match
+			actual := c
+			expected := deque[dPtr]
+			if actual == expected {
+				dPtr--
+			} else {
+				panic("this should not be processing corrupt lines")
+			}
+		}
+	}
+	reverse(deque[:dPtr+1])
+	return deque[:dPtr+1]
+}
+
+func reverse[S ~[]E, E any](s S) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
 }
 
 func score(badChar rune) int {
@@ -64,18 +100,44 @@ func score(badChar rune) int {
 	}
 }
 
+func autoScore(chars []rune) int {
+	iscore := 0
+	for _, c := range chars {
+		iscore *= 5
+		switch c {
+		case ')':
+			iscore += 1
+		case ']':
+			iscore += 2
+		case '}':
+			iscore += 3
+		case '>':
+			iscore += 4
+		default:
+			panic(c)
+		}
+	}
+	return iscore
+}
+
 func part1(input []string) int {
 	errs := f8l.Map[string, rune](&input, firstSyntaxError)
-	fmt.Println(errs)
+	//fmt.Println(errs)
 	scores := f8l.Map[rune, int](&errs, score)
-	fmt.Println(scores)
+	//fmt.Println(scores)
 	totalErrorScore := f8l.Reduce[int](&scores, 0, func(a int, b int) int { return a + b })
-	assert.Equal(totalErrorScore, 265527)
+	assert.EqualAny(totalErrorScore, []int{26397, 265527}, "total error score")
 	return totalErrorScore
 }
 
 func part2(input []string) int {
-	//assert.NotEqual(totalFish, 5934)
-	//assert.EqualAny(best, []int{168, 97038163}, "best")
-	return -1
+	// Now, discard the corrupted lines. The remaining lines are incomplete.
+	lines := f8l.Filter(&input, func(line string) bool { return ' ' == firstSyntaxError(line) })
+	scores := f8l.Map[string, int](&lines, func(line string) int {
+		return autoScore(autocomplete(line))
+	})
+	sort.Ints(scores)
+	middleScore := scores[len(scores)/2]
+	assert.EqualAny(middleScore, []int{288957, 3969823589}, "middle score")
+	return middleScore
 }
