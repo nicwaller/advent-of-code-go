f2896d1e9d5dccd2dcfde41833af6a4d028ee81d
Nic Waller <code@nicwaller.com>
2022-11-12 21:03:45 -0800

    day 6, part 2

--- day06/day06.go
+++ day06/day06.go
@@ -25,7 +25,7 @@ func part1(input fileType) int {
 	for _, v := range input {
 		fish[v]++
 	}
-	for day := 1; day <= 80; day++ {
+	for day := 1; day <= 256; day++ {
 		newFish := fish[0]
 		for i := 0; i < 8; i++ {
 			fish[i] = fish[i+1]
@@ -36,7 +36,7 @@ func part1(input fileType) int {
 	totalFish := f8l.Sum(&fish)
 	assert.NotEqual(totalFish, 1711)
 	assert.NotEqual(totalFish, 5934)
-	assert.EqualAny(totalFish, []int{26, 350605}, "total fish")
+	assert.EqualAny(totalFish, []int{26, 350605, 1592778185024}, "total fish")
 	return totalFish
 }
 
