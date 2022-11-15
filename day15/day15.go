996541da9a7e40e38f029a26aef953c402b5fe75
Nic Waller <code@nicwaller.com>
2022-11-15 00:32:09 -0800

    day 15 refactor

--- day15/day15.go
+++ day15/day15.go
@@ -3,60 +3,60 @@ package main
 import (
 	"advent-of-code/lib/aoc"
 	"advent-of-code/lib/grid"
-	"fmt"
 	"github.com/yourbasic/graph"
+	"strconv"
 )
 
 func main() {
-	//aoc.UseSampleFile()
-	fmt.Printf("Part 1: %d\n", part1(parseFile()))
-	fmt.Printf("Part 2: %d\n", part2(parseFile()))
+	aoc.Day(15)
+	aoc.Test(run, "sample.txt", "40", "315")
+	aoc.Test(run, "input.txt", "363", "2835")
+	aoc.Run(run)
 }
 
-func parseFile() grid.Grid[int] {
-	return grid.FromStringAsInt(aoc.InputString())
+func run(p1 *string, p2 *string) {
+	smallGrid := grid.FromStringAsInt(aoc.InputString())
+
+	smallGraph := GridToGraph(smallGrid)
+	_, distance := graph.ShortestPath(smallGraph, 0, smallGrid.RowCount()*smallGrid.ColumnCount()-1)
+	part1 := int(distance)
+	*p1 = strconv.Itoa(part1)
+
+	bigGrid := expandGrid(smallGrid)
+	bigGraph := GridToGraph(bigGrid)
+	_, distance = graph.ShortestPath(bigGraph, 0, bigGrid.RowCount()*bigGrid.ColumnCount()-1)
+	part2 := int(distance)
+	*p2 = strconv.Itoa(part2)
 }
 
-func part1(input grid.Grid[int]) int {
-	g := graph.New(input.RowCount() * input.ColumnCount())
-	for c := input.Cells(); c.Next(); {
-		dst := input.OffsetFromCell(c.Value())
-		for _, n := range input.NeighboursAdjacent(c.Value(), false) {
-			src := input.OffsetFromCell(n)
-			g.AddCost(src, dst, int64(input.Get(c.Value())))
-		}
-	}
-	_, distance := graph.ShortestPath(g, 0, input.RowCount()*input.ColumnCount()-1)
-	return int(distance)
-}
-
-func part2(inputA grid.Grid[int]) int {
-	bigGrid := grid.NewGrid[int](inputA.RowCount()*5, inputA.ColumnCount()*5)
+func expandGrid(smallGrid grid.Grid[int]) grid.Grid[int] {
+	bigGrid := grid.NewGrid[int](smallGrid.RowCount()*5, smallGrid.ColumnCount()*5)
 	for dstCellIter := bigGrid.Cells(); dstCellIter.Next(); {
 		dstCell := dstCellIter.Value()
 		srcCell := grid.Cell{
-			dstCell[0] % inputA.Width(),
-			dstCell[1] % inputA.Height(),
+			dstCell[0] % smallGrid.Width(),
+			dstCell[1] % smallGrid.Height(),
 		}
-		riskOffset := dstCell[0]/inputA.Width() + dstCell[1]/inputA.Height()
-		srcVal := inputA.Get(srcCell)
+		riskOffset := dstCell[0]/smallGrid.Width() + dstCell[1]/smallGrid.Height()
+		srcVal := smallGrid.Get(srcCell)
 		// this line here is a bit of straight fuckery
 		// y u do this to me
 		// > "However, risk levels above 9 wrap back around to 1"
 		// SERIOUSLY?!
 		dstVal := (srcVal+riskOffset)%10 + ((srcVal + riskOffset) / 10)
-		_ = riskOffset
 		bigGrid.Set(dstCell, dstVal)
 	}
-	bigGrid.Print()
-	g := graph.New(bigGrid.RowCount() * bigGrid.ColumnCount())
-	for c := bigGrid.Cells(); c.Next(); {
-		dst := bigGrid.OffsetFromCell(c.Value())
-		for _, n := range bigGrid.NeighboursAdjacent(c.Value(), false) {
-			src := bigGrid.OffsetFromCell(n)
-			g.AddCost(src, dst, int64(bigGrid.Get(c.Value())))
+	return bigGrid
+}
+
+func GridToGraph(grd grid.Grid[int]) *graph.Mutable {
+	graf := graph.New(grd.RowCount() * grd.ColumnCount())
+	for c := grd.Cells(); c.Next(); {
+		dst := grd.OffsetFromCell(c.Value())
+		for _, n := range grd.NeighboursAdjacent(c.Value(), false) {
+			src := grd.OffsetFromCell(n)
+			graf.AddCost(src, dst, int64(grd.Get(c.Value())))
 		}
 	}
-	_, distance := graph.ShortestPath(g, 0, bigGrid.RowCount()*bigGrid.ColumnCount()-1)
-	return int(distance)
+	return graf
 }
