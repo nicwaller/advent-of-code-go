2b1690e536bddf2c05cb3f916c4b36796bedb6d5
Nic Waller <code@nicwaller.com>
2022-11-20 19:20:51 -0800

    2015.1

--- 2015/day01/day01.go
+++ 2015/day01/day01.go
@@ -1,18 +1,34 @@
 package main
 
 import (
+	"advent-of-code/lib/analyze"
 	"advent-of-code/lib/aoc"
+	"advent-of-code/lib/iter"
 	"strconv"
 )
 
 func main() {
 	aoc.Select(2015, 1)
 	aoc.Test(run, "sample.txt", "", "")
-	aoc.Test(run, "input.txt", "", "")
+	aoc.Test(run, "input.txt", "280", "1797")
 	aoc.Run(run)
 }
 
 func run(p1 *string, p2 *string) {
-	*p1 = strconv.Itoa(0)
-	*p2 = strconv.Itoa(0)
+	a := analyze.CountDistinct(iter.StringIterator(aoc.InputString(), 1).List())
+	*p1 = strconv.Itoa(a["("] - a[")"])
+
+	level := 0
+	for pos, x := range aoc.InputString() {
+		switch x {
+		case '(':
+			level++
+		case ')':
+			level--
+		}
+		if level < 0 {
+			*p2 = strconv.Itoa(pos + 1)
+			break
+		}
+	}
 }
