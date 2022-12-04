f2ac32cbe05e9b6e4ef082c9418d77301f4af9ae
Nic Waller <code@nicwaller.com>
2022-12-04 11:46:51 -0800

    2022.3

--- 2022/day03/day03.go
+++ 2022/day03/day03.go
@@ -2,17 +2,50 @@ package main
 
 import (
 	"advent-of-code/lib/aoc"
+	"advent-of-code/lib/iter"
+	"advent-of-code/lib/set"
 	"strconv"
 )
 
 func main() {
 	aoc.Select(2022, 3)
-	aoc.Test(run, "sample.txt", "", "")
-	aoc.Test(run, "input.txt", "", "")
+	aoc.Test(run, "sample.txt", "157", "70")
+	aoc.Test(run, "input.txt", "8088", "")
 	aoc.Run(run)
 }
 
+func itemPriority(s string) uint8 {
+	isLowercase := (s[0] & 0b00100000) != 0
+	inPlaneVal := s[0] & 0b00011111
+	if isLowercase {
+		return inPlaneVal
+	} else {
+		return inPlaneVal + 26
+	}
+}
+
 func run(p1 *string, p2 *string) {
-	*p1 = strconv.Itoa(0)
-	*p2 = strconv.Itoa(0)
+	pSum := 0
+	for _, packingList := range aoc.InputLines() {
+		rucksack1Str := packingList[0 : len(packingList)/2]
+		rucksack2Str := packingList[len(packingList)/2:]
+		rucksack1 := set.FromIterable(iter.StringIterator(rucksack1Str, 1))
+		rucksack2 := set.FromIterable(iter.StringIterator(rucksack2Str, 1))
+		ix := set.Intersection(rucksack1, rucksack2)
+		pSum += int(itemPriority(ix.Items()[0]))
+	}
+	*p1 = strconv.Itoa(pSum)
+
+	groupIter := iter.Chunk[string](3, aoc.InputLinesIterator())
+	pSum2 := 0
+	for groupIter.Next() {
+		group := groupIter.Value()
+		ix2 := set.Intersection(
+			set.FromIterable(iter.StringIterator(group[0], 1)),
+			set.FromIterable(iter.StringIterator(group[1], 1)),
+			set.FromIterable(iter.StringIterator(group[2], 1)),
+		)
+		pSum2 += int(itemPriority(ix2.Items()[0]))
+	}
+	*p2 = strconv.Itoa(pSum2)
 }
