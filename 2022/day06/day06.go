a97cf35c230e60045823145a703f5d50051c6a81
Nic Waller <code@nicwaller.com>
2022-12-06 10:42:29 -0800

    2022.6 fast

--- 2022/day06/day06.go
+++ 2022/day06/day06.go
@@ -1,18 +1,44 @@
 package main
 
 import (
+	"advent-of-code/lib/analyze"
 	"advent-of-code/lib/aoc"
+	"advent-of-code/lib/iter"
 	"strconv"
 )
 
 func main() {
 	aoc.Select(2022, 6)
-	aoc.Test(run, "sample.txt", "", "")
+	aoc.Test(run, "sample.txt", "7", "")
 	aoc.Test(run, "input.txt", "", "")
 	aoc.Run(run)
+	aoc.Out()
 }
 
 func run(p1 *string, p2 *string) {
-	*p1 = strconv.Itoa(0)
-	*p2 = strconv.Itoa(0)
+	// find four characters all different
+	// how many characters before marker?
+
+	ite := iter.SlidingWindow(4, iter.StringIterator(aoc.InputString(), 1))
+	i := 4
+	for ite.Next() {
+		//abc := strings.Join(ite.Value(), "")
+		if analyze.Analyze(ite.Value()).Distinct == 4 {
+			*p1 = strconv.Itoa(i)
+			break
+		}
+		i++
+	}
+
+	ite2 := iter.SlidingWindow(14, iter.StringIterator(aoc.InputString(), 1))
+	i2 := 14
+	for ite2.Next() {
+		//abc := strings.Join(ite.Value(), "")
+		if analyze.Analyze(ite2.Value()).Distinct == 14 {
+			*p2 = strconv.Itoa(i2)
+			break
+		}
+		i2++
+	}
+
 }
