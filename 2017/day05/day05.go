bfe31583b4b7e670aa3ccf9cd00871fcccaf8b82
Nic Waller <code@nicwaller.com>
2022-12-10 13:08:00 -0800

    2017.5

--- 2017/day05/day05.go
+++ 2017/day05/day05.go
@@ -7,12 +7,42 @@ import (
 
 func main() {
 	aoc.Select(2017, 5)
-	aoc.Test(run, "sample.txt", "", "")
-	aoc.Test(run, "input.txt", "", "")
+	aoc.Test(run, "sample.txt", "5", "")
+	//aoc.Test(run, "input.txt", "", "")
 	aoc.Run(run)
+	aoc.Out()
 }
 
 func run(p1 *string, p2 *string) {
-	*p1 = strconv.Itoa(0)
-	*p2 = strconv.Itoa(0)
+	tape := aoc.InputLinesInt()
+	pc := 0
+	steps := 0
+	for {
+		jmpOffset := tape[pc]
+		tape[pc]++
+		pc += jmpOffset
+		steps++
+		if pc < 0 || pc >= len(tape) {
+			break
+		}
+	}
+	*p1 = strconv.Itoa(steps)
+
+	tape = aoc.InputLinesInt()
+	pc = 0
+	steps = 0
+	for {
+		jmpOffset := tape[pc]
+		if jmpOffset >= 3 {
+			tape[pc]--
+		} else {
+			tape[pc]++
+		}
+		pc += jmpOffset
+		steps++
+		if pc < 0 || pc >= len(tape) {
+			break
+		}
+	}
+	*p2 = strconv.Itoa(steps)
 }
