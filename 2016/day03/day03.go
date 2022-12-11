07b74f23e81b2b89c7f2767806bd7c6f71984cff
Nic Waller <code@nicwaller.com>
2022-12-11 11:44:21 -0800

    2016.3

--- 2016/day03/day03.go
+++ 2016/day03/day03.go
@@ -2,17 +2,43 @@ package main
 
 import (
 	"advent-of-code/lib/aoc"
+	"advent-of-code/lib/iter"
+	"advent-of-code/lib/util"
+	"sort"
 	"strconv"
 )
 
 func main() {
 	aoc.Select(2016, 3)
-	aoc.Test(run, "sample.txt", "", "")
-	aoc.Test(run, "input.txt", "", "")
+	//aoc.Test(run, "input.txt", "982", "1826")
 	aoc.Run(run)
 }
 
+func isTriangle(edgeLength []int) bool {
+	sort.Ints(edgeLength)
+	return edgeLength[0]+edgeLength[1] > edgeLength[2]
+}
+
 func run(p1 *string, p2 *string) {
-	*p1 = strconv.Itoa(0)
-	*p2 = strconv.Itoa(0)
+	count := 0
+	for _, line := range aoc.InputLines() {
+		if isTriangle(util.NumberFields(line)) {
+			count++
+		}
+	}
+	*p1 = strconv.Itoa(count)
+
+	count = 0
+	blocks := iter.Chunk(3, aoc.InputLinesIterator())
+	for blocks.Next() {
+		v := blocks.Value()
+		nf := util.NumberFields(v[0] + v[1] + v[2])
+		for offset := 0; offset < 3; offset++ {
+			t := []int{nf[offset+0], nf[offset+3], nf[offset+6]}
+			if isTriangle(t) {
+				count++
+			}
+		}
+	}
+	*p2 = strconv.Itoa(count)
 }
