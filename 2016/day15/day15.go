8dd1e90173e1d4f4ccda3d324f1896217e17a8c2
Nic Waller <code@nicwaller.com>
2022-12-12 18:45:46 -0800

    2016.15

--- 2016/day15/day15.go
+++ 2016/day15/day15.go
@@ -2,17 +2,51 @@ package main
 
 import (
 	"advent-of-code/lib/aoc"
+	"advent-of-code/lib/util"
 	"strconv"
 )
 
 func main() {
 	aoc.Select(2016, 15)
-	aoc.Test(run, "sample.txt", "", "")
-	aoc.Test(run, "input.txt", "", "")
+	aoc.Test(run, "sample.txt", "5", "")
+	aoc.Test(run, "input.txt", "317371", "2080951")
 	aoc.Run(run)
 }
 
 func run(p1 *string, p2 *string) {
-	*p1 = strconv.Itoa(0)
-	*p2 = strconv.Itoa(0)
+	discSize := make([]int, 0)
+	discStartingPos := make([]int, 0)
+	for _, line := range aoc.InputLines() {
+		nf := util.NumberFields(line)
+		discSize = append(discSize, nf[1])
+		discStartingPos = append(discStartingPos, nf[3])
+	}
+
+	leastSafeDelay := func() int {
+		discPos := make([]int, len(discStartingPos))
+		rotate := func(n int) {
+			for i, _ := range discPos {
+				discPos[i] += n
+				discPos[i] %= discSize[i]
+			}
+		}
+	delayLoop:
+		for delay := 0; ; delay++ {
+			copy(discPos, discStartingPos)
+			rotate(delay)
+			// follow the capsule through the discs
+			for step := 0; step < len(discPos); step++ {
+				rotate(1)
+				if discPos[step] != 0 {
+					continue delayLoop
+				}
+			}
+			return delay
+		}
+	}
+
+	*p1 = strconv.Itoa(leastSafeDelay())
+	discSize = append(discSize, 11)
+	discStartingPos = append(discStartingPos, 0)
+	*p2 = strconv.Itoa(leastSafeDelay())
 }
