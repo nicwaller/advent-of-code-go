718315f4a0955afe47b26d17f92734b67af83d2d
Nic Waller <code@nicwaller.com>
2022-12-14 09:21:05 -0800

    2022.14 automatic bounds

--- 2022/day14/day14.go
+++ 2022/day14/day14.go
@@ -1,52 +1,70 @@
 package main
 
 import (
+	"advent-of-code/lib/analyze"
 	"advent-of-code/lib/aoc"
 	"advent-of-code/lib/grid"
+	"advent-of-code/lib/iter"
 	"advent-of-code/lib/util"
 	"strconv"
 )
 
-var gs grid.Slice
-
 func main() {
 	aoc.Select(2022, 14)
-	gs = grid.SliceEnclosing(
-		grid.Cell{0, 500},
-		grid.Cell{9, 494},
-		grid.Cell{9, 503},
-	)
 	aoc.Test(run, "sample.txt", "24", "93")
-	gs = grid.SliceEnclosing(
-		grid.Cell{0, 500},
-		grid.Cell{300, 100},
-		grid.Cell{300, 900},
-	)
-	//aoc.Test(run, "input.txt", "808", "26625")
-	aoc.Run(run)
+	aoc.Test(run, "input.txt", "808", "26625")
+	//aoc.Run(run)
+	aoc.Out()
 }
 
 var maxY int
 
-func parse() grid.Grid[string] {
-	g := grid.NewGridFromSlice[string](gs)
-	g.Fill(".")
+func parse(includeFloor bool) grid.Grid[string] {
+	lineSegments := make([][]grid.Cell, 0)
+	scanX := analyze.Box[int]{}
+	scanY := analyze.Box[int]{}
 	for _, line := range aoc.InputLines() {
 		nf := util.NumberFields(line)
+		for i := 0; i < len(nf); i += 2 {
+			scanX.Put(nf[i])
+			scanY.Put(nf[i+1])
+		}
 		for i := 0; i < len(nf)-2; i += 2 {
 			c0 := grid.Cell{nf[i+1], nf[i+0]}
 			c1 := grid.Cell{nf[i+3], nf[i+2]}
-			for _, c := range grid.Line(c0, c1).List() {
-				maxY = util.IntMax(maxY, c[0])
-				g.Set(c, "#")
-			}
+			lineSegments = append(lineSegments, []grid.Cell{c0, c1})
 		}
+	}
+	if includeFloor {
+		scanY.Put(scanY.Max + 2)
+	}
+	g := grid.NewGridFromSlice[string](grid.SliceEnclosing(
+		grid.Cell{0, 500}, // sand origin
+		grid.Cell{scanY.Max, scanX.Min - 1 - scanY.Max},
+		grid.Cell{scanY.Max, scanX.Max + 1 + scanY.Max},
+	))
+	g.Fill(".")
+	for _, lineSegment := range lineSegments {
+		for _, c := range grid.Line(util.Pair(lineSegment)).List() {
+			maxY = util.IntMax(maxY, c[0])
+			g.Set(c, "#")
+		}
+	}
+	if includeFloor {
+		floorCentre := grid.Cell{scanY.Max, 500}
+		g.Set(floorCentre, "#")
+		iter.Chain(
+			g.Ray(floorCentre, grid.Cell{0, -1}),
+			g.Ray(floorCentre, grid.Cell{0, 1}),
+		).Each(func(c grid.Cell) {
+			g.Set(c, "#")
+		})
 	}
 	return g
 }
 
 func run(p1 *string, p2 *string) {
-	g := parse()
+	g := parse(false)
 	origin := grid.Cell{0, 500}
 	restingPlace := func(start grid.Cell) grid.Cell {
 		p := util.Copy(start)
@@ -80,19 +98,7 @@ func run(p1 *string, p2 *string) {
 		}
 	}
 
-	g = parse()
-	// floor!
-	g.Grow(10, ".")
-	left := grid.Cell{0, -1}
-	right := grid.Cell{0, 1}
-	floorCentre := grid.Cell{maxY + 2, 500}
-	g.Set(floorCentre, "#")
-	for rayLeft := g.Ray(floorCentre, left); rayLeft.Next(); {
-		g.Set(rayLeft.Value(), "#")
-	}
-	for rayRight := g.Ray(floorCentre, right); rayRight.Next(); {
-		g.Set(rayRight.Value(), "#")
-	}
+	g = parse(true)
 	for grains := 0; ; {
 		grains++
 		rp := restingPlace(origin)
