a7bf47054d46073009bf55958cd1e87111d2e71f
Nic Waller <code@nicwaller.com>
2023-12-04 21:59:02 -0800

    2023.5.2 brute force

--- 2023/day05/day05.go
+++ 2023/day05/day05.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"advent-of-code/lib/aoc"
+	"advent-of-code/lib/iterc"
 	"advent-of-code/lib/util"
 	"fmt"
 	"math"
@@ -12,7 +13,7 @@ import (
 func main() {
 	aoc.Select(2023, 5)
 	aoc.Test(run, "sample.txt", "35", "46")
-	aoc.Test(run, "input.txt", "825516882", "")
+	aoc.Test(run, "input.txt", "825516882", "136096660")
 	aoc.Run(run)
 	aoc.Out()
 }
@@ -26,22 +27,21 @@ func run(p1 *string, p2 *string) {
 
 	fmt.Println("seeds:", seeds)
 
-	maps := make([][]func(string) (string, bool), 0)
+	maps := make([][]func(uint64) (uint64, bool), 0)
 	for _, pg := range pgs {
 		//ff := strings.Fields(pg[0])
 		//mapName := ff[0]
-		mapsInner := make([]func(string) (string, bool), 0)
+		mapsInner := make([]func(uint64) (uint64, bool), 0)
 		for i := 1; i < len(pg); i++ {
 			fff := util.NumberFields(pg[i])
-			srcRangeStart := fff[1]
-			destRangeStart := fff[0]
-			rangeLength := fff[2]
-			mapsInner = append(mapsInner, func(s string) (string, bool) {
-				v := util.UnsafeAtoi(s)
+			srcRangeStart := uint64(fff[1])
+			destRangeStart := uint64(fff[0])
+			rangeLength := uint64(fff[2])
+			mapsInner = append(mapsInner, func(v uint64) (uint64, bool) {
 				if v >= srcRangeStart && v <= srcRangeStart+rangeLength {
-					return strconv.Itoa(destRangeStart + (v - srcRangeStart)), true
+					return destRangeStart + (v - srcRangeStart), true
 				} else {
-					return s, false
+					return v, false
 				}
 			})
 		}
@@ -49,9 +49,9 @@ func run(p1 *string, p2 *string) {
 	}
 
 	var found bool
-	var lowest uint64 = math.MaxInt64
+	var lowest1 uint64 = math.MaxInt64
 	for _, seed := range seeds {
-		v := seed
+		v := uint64(util.UnsafeAtoi(seed))
 		for _, m := range maps {
 		transforms:
 			for _, transform := range m {
@@ -63,24 +63,38 @@ func run(p1 *string, p2 *string) {
 			//	v =v
 			//}
 		}
-		lowest = min(lowest, uint64(util.UnsafeAtoi(v)))
+		lowest1 = min(lowest1, v)
 		fmt.Println("seed", seed, v)
 	}
 
-	//iterc.SlidingWindow(iterc.ListIterator(seeds), 2).ForEach(func(w []string) {
-	//	offset := util.UnsafeAtoi(w[0])
-	//	rangelen := util.UnsafeAtoi(w[1])
-	//	terminus := offset + rangelen
-	//	for i := offset; i < terminus; i++ {
-	//		if i%1000000 == 0 {
-	//			fmt.Println("million") // 20,000 of these
-	//		}
-	//
-	//	}
-	//})
+	var lowest2 uint64 = math.MaxInt64
+	iterc.Chunk(iterc.ListIterator(seeds), 2).ForEach(func(w []string) {
+		if len(w) == 0 {
+			return
+		}
+		offset := uint64(util.UnsafeAtoi(w[0]))
+		rangelen := uint64(util.UnsafeAtoi(w[1]))
+		terminus := offset + rangelen
+		var i uint64
+		for i = offset; i < terminus; i++ {
+			if i%10000000 == 0 {
+				fmt.Println("10million") // 20,000 of these
+			}
+			v := i
+			for _, m := range maps {
+			transforms:
+				for _, transform := range m {
+					if v, found = transform(v); found {
+						break transforms
+					}
+				}
+			}
+			lowest2 = min(lowest2, v)
+		}
+	})
 
-	*p1 = strconv.Itoa(int(lowest))
-	*p2 = strconv.Itoa(0)
+	*p1 = strconv.Itoa(int(lowest1))
+	*p2 = strconv.Itoa(int(lowest2))
 }
 
 //func (d *DefaultMap) Get(k string) string {
