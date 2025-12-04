f04d77e978088576ce00a5f586b07386dc10d6f3
Nic Waller <code@nicwaller.com>
2025-12-03 21:10:51 -0800

    2025.04 part two

--- 2025/day04/day04.go
+++ 2025/day04/day04.go
@@ -9,8 +9,8 @@ import (
 
 func main() {
 	aoc.Select(2025, 4)
-	aoc.Test(run, "sample.txt", "13", "1395")
-	aoc.Test(run, "input.txt", "", "")
+	aoc.Test(run, "sample.txt", "13", "43")
+	aoc.Test(run, "input.txt", "1395", "8451")
 	aoc.Run(run)
 }
 
@@ -29,14 +29,39 @@ func run(p1 *string, p2 *string) {
 			}
 		}
 		if count < 4 {
-			fmt.Println(cell)
 			accessible++
 		}
 	}
-	g.Print()
 
-	//for _, line := range aoc.InputLines() {
-	//}
+	removedTotal := 0
+cleanup:
+	for {
+		removed := 0
+
+		for _, cell := range g.Cells().List() {
+			if g.Get(cell) == "." {
+				continue
+			}
+
+			count := 0
+			for _, n := range g.NeighboursSurround(cell, false) {
+				if g.Get(n) == "@" {
+					count++
+				}
+			}
+			if count < 4 {
+				g.Set(cell, ".")
+				removed++
+			}
+		}
+
+		fmt.Printf("removed this round: %d\n", removed)
+		removedTotal += removed
+		if removed == 0 {
+			break cleanup
+		}
+	}
+
 	*p1 = strconv.Itoa(accessible)
-	*p2 = ""
+	*p2 = strconv.Itoa(removedTotal)
 }
