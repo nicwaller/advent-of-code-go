63dbc7100b3b38d3e6bf54fd5a3fce036cd11507
Nic Waller <code@nicwaller.com>
2025-12-07 10:39:42 -0800

    2025.07 tidy up

--- 2025/day07/day07.go
+++ 2025/day07/day07.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
 	"iter"
-	"slices"
 	"strconv"
 
 	"advent-of-code/lib/aoc"
@@ -20,112 +18,71 @@ func main() {
 }
 
 const (
-	EmptySpace       = '.'
-	StartingManifold = 'S'
-	TachyonBeam      = '|'
-	BeamSplitter     = '^'
+	EmptySpace    = '.'
+	StartingPoint = 'S'
+	TachyonBeam   = '|'
+	BeamSplitter  = '^'
 )
 
 type Beam = iter.Seq[grid2.Cell[rune]]
 
 func run(p1 *string, p2 *string) {
+	*p1 = strconv.Itoa(countActivatedBeamSplitters())
+	*p2 = strconv.Itoa(countMultiverses())
+}
+
+func countActivatedBeamSplitters() int {
 	g := grid2.NewGridFromString(aoc.InputString())
-	*p1 = strconv.Itoa(approach1(g))
 
-	g2 := grid2.NewGridFromString(aoc.InputString())
-	*p2 = strconv.Itoa(partTwo(g2))
+	// find the starting point "S"
+	start := util.IterNext(g.Select().ValueEquals(StartingPoint))
+
+	// then launch a single tachyon beam and keep track of how many beam splitters we encounter
+	splitCount := 0
+	propagateTachyonBeam(g, start.Neighbour(grid2.Down), &splitCount)
+
+	return splitCount
 }
 
-func approach1(g *grid2.Grid[rune]) int {
-	beams := make([]Beam, 0)
-
-	launchBeam := func(src grid2.Cell[rune]) {
-		switch src.Get() {
-		case EmptySpace:
-			src.Set(TachyonBeam)
-			beams = append(beams, src.Ray(grid2.Down))
-		case TachyonBeam:
-			// launch failed, does nothing
-			return
-		case BeamSplitter:
-			panic("I did not expect this")
-		default:
-			panic("what other possibility is there?")
-		}
+// depth-first traversal is fun and easy!
+func propagateTachyonBeam(g *grid2.Grid[rune], at grid2.Cell[rune], splitCount *int) {
+	if !at.Coordinates().In(g.All().Bounds()) {
+		return
 	}
-
-	// first, find the starting point "S" and launch the tachyon beam
-	start := util.IterNext(g.Select().ValueEquals(StartingManifold))
-	launchBeam(start.Neighbour(grid2.Down))
-
-	// next, perpetuate the tachyon beams
-	for keepGoing := true; keepGoing; {
-		keepGoing = false
-		for _, beam := range beams {
-			nextCell := util.IterNext(beam)
-			if nextCell == nil {
-				// this beam is complete
-				continue
-			} else {
-				// at least one beam is continuing
-				keepGoing = true
-			}
-			ncv := nextCell.Get()
-			switch ncv {
-			case EmptySpace:
-				// propagate the tachyon beam
-				nextCell.Set(TachyonBeam)
-			case BeamSplitter:
-				// halt this beam
-				_ = slices.Collect(beam)
-				// make two more beams
-				launchBeam(nextCell.Neighbour(grid2.Left))
-				launchBeam(nextCell.Neighbour(grid2.Right))
-			case TachyonBeam:
-				// do nothing, already a beam here
-				continue
-			default:
-				panic(fmt.Errorf("cell contains %q", ncv))
-			}
-		}
+	switch at.Get() {
+	case EmptySpace:
+		at.Set(TachyonBeam)
+		propagateTachyonBeam(g, at.Neighbour(grid2.Down), splitCount)
+	case BeamSplitter:
+		*splitCount++
+		propagateTachyonBeam(g, at.Neighbour(grid2.Left), splitCount)
+		propagateTachyonBeam(g, at.Neighbour(grid2.Right), splitCount)
+	case TachyonBeam:
+		// do nothing
+		return
+	case StartingPoint:
+		panic("this should be impossible")
 	}
-
-	// NOTE: I'm not trying to find "how many beams split"
-	// I'm trying to find "how many beam splitters are activated"
-	// in other words, finding beam splitters that never have a beam reach them.
-	// So let's find all the splitters that don't have a tachyon beam | directly above.
-
-	splits := 0
-	for splitterCell := range g.Select().ValueEquals(BeamSplitter) {
-		aboveCell := splitterCell.Neighbour(grid2.Up)
-		if aboveCell.Get() == TachyonBeam {
-			splits++
-		}
-	}
-
-	return splits
 }
 
-func partTwo(g *grid2.Grid[rune]) int {
+func countMultiverses() int {
+	g := grid2.NewGridFromString(aoc.InputString())
 	particlesPerColumn := make([]int, g.All().Bounds().Dx()) // assumes origin at 0,0
 
-	// first, find the starting point "S" and launch the tachyon beam
-	startingCell := util.IterNext(g.Select().ValueEquals(StartingManifold))
-	particlesPerColumn[startingCell.Coordinates().X]++
+	// first, find the starting point "S" and launch a single tachyon beam
+	g.Select().ValueEquals(StartingPoint)(func(start grid2.Cell[rune]) bool {
+		particlesPerColumn[start.Coordinates().X]++
+		return false // halt search after finding the first manifold (there should only be one)
+	})
 
-	// now proceed row by row
-	for y, row := range slices.Collect(g.Rows()) {
-		for x, r := range row {
-			if r == BeamSplitter {
-				fmt.Printf("activated beam splitter at [%d, %d]\n", x, y)
-				fmt.Println(particlesPerColumn)
-				// this is gonna fail with out of bounds, I know it
-				particlesPerColumn[x-1] += particlesPerColumn[x]
-				particlesPerColumn[x+1] += particlesPerColumn[x]
-				particlesPerColumn[x] = 0
-			}
-		}
-	}
+	// for each beam splitter, update the count of multiverses
+	g.Select().ValueEquals(BeamSplitter)(func(start grid2.Cell[rune]) bool {
+		x := start.Coordinates().X
+		particlesPerColumn[x-1] += particlesPerColumn[x]
+		particlesPerColumn[x+1] += particlesPerColumn[x]
+		particlesPerColumn[x] = 0
+		return true
+	})
 
 	return f8l.Reduce(particlesPerColumn, 0, util.IntSum)
 }
